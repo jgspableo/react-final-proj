01f97364157ec9fd5844786a064899951cb27b47
Julius Pableo <jgspableo@outlook.com>
2025-12-11 20:51:23 +0800

    Build site for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-final-proj",
   plugins: [react()],
-})
+  base: "/react-final-proj/",
+  build: {
+    outDir: "docs", 
+  },
+});
