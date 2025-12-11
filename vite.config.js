c53b63a6efc7f6f99f7a716f2ccaaad0bf54771a
Julius Pableo <jgspableo@outlook.com>
2025-12-11 20:42:51 +0800

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React/final proj/",
+  base: "/react-final-proj",
   plugins: [react()],
 })
